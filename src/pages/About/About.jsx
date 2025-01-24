70bfd0b07dca752780a42e1205e476b64b5f16f1
ADITYA BASANTIA <adityabasantia729@gmail.com>
2025-01-24 12:21:33 +0530

    port

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,19 +3,6 @@ import LetsTalk from "../../components/PortfolioComponent/LetsTalk";
 
 const About = () => {
   <div className="About">
-    <div className="BrandingHerosec container-fluid">
-      <div className="row">
-        <div className="col-md-12 d-flex align-items-center justify-content-center">
-          <div className="BrandHeading">
-            <h1>A Glimpse Into Our Domain</h1>
-            <p>
-              Design creates culture. Culture shapes values. Values determine
-              the future.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
     <LetsTalk
       firstText="Let's redefine your brand together"
       secondText="We cover it all"
